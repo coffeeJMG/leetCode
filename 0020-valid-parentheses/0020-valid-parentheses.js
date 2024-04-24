14d2b811fbbc22772dcf1ebb3ac2b6434e34a23e
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-04-24 23:36:29 +0900

    Time: 62 ms (32.92%), Space: 50.6 MB (29.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,22 +2,25 @@ var isValid = function(s) {
     let stack = [];
     let valids = {'(':')', '{':'}', '[':']'};
 
+    if(s.length % 2 === 1){
+        return false; // 홀수 길이는 바로 false 반환
+    }
+    
     for(let i = 0; i < s.length; i++){
         let item = s[i];
 
         if (valids[item]) {
             stack.push(item);
-        } else { 
+        } else {
             if (stack.length === 0) {
-                return false; // 스택이 비어있으면 짝이 맞지 않음
+                return false; // 스택이 비어있으면 닫는 괄호가 먼저 나온 것
             }
-
             let last = stack.pop(); 
             if (valids[last] !== item) {
-                return false; 
+                return false; // 짝이 맞지 않음
             }
         }
     }
 
-    return stack.length === 0; // 스택이 비어있어야 모든 괄호가 짝이 맞음
+    return stack.length === 0; // 스택이 비어 있어야 모든 괄호가 짝이 맞음
 };
