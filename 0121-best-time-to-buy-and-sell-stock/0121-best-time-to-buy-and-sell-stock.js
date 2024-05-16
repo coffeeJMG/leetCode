c81651a65f748b4b53fa9f044cfd4ed7d584ab0d
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-05-16 16:44:06 +0900

    Time: 89 ms (7.96%), Space: 58.6 MB (84.16%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,52 +4,24 @@
  */
 var maxProfit = function(prices) {
 
-        
-        let current = 99999999999;
-        let result = 0; // i번째 날짜의 예상 판매수익 
-        
-        
-        for(let i=0; i<prices.length-1; i++){
-                
-                
-                
-                let selledPrice = 0; // 매 날짜마다 가능한 매도 가격 리셋
-                
-            
-                let buyPrice = prices[i]
-                
-                if(buyPrice < current){
-                        current= buyPrice
-                       for(let j=i+1; j<prices.length; j++){
-                        
-                        let sellPrice = prices[j]
-                        
-                        if(sellPrice > buyPrice && sellPrice-buyPrice > selledPrice ){
-                            
-                            selledPrice = sellPrice-buyPrice
-                            
-                             console.log(selledPrice,i,j, "판매 가능한 날짜 존재 ")
-                        }
-                    
-                   
-                        
-                } 
-                
-                if(selledPrice >=result){
-                    
-                    
-                    result = selledPrice
-                    console.log(result, "초과 수익 생성")
-                }
-            
-            }
-        
-                }
-                
-                
-             
-        
-        return result
+        max =0;
+        min = 9999999999999999;
+        maxdata = 0;
+    
+      for(let i=0; i<prices.length; i++){
+          
+          let price= prices[i]
+          
+          if(price <min){
+              min = price
+          }else if(price-min > max && price>maxdata){
+              
+              max = price-min
+              maxdata-price
+          }
+      }
+    
+    return max
     
 };
 
@@ -58,6 +30,25 @@ var maxProfit = function(prices) {
 // 배열을 돌면서 나보다 크다 &&  sellPrice 보다 크면 sellPrice 에 저장
 // 매 i 번째마다 sellPrice 가 result 보다 크면 result 갱신
 
+// var maxProfit = function(prices) {
+//     let minPrice = Infinity; // 초기 최소 가격을 무한대로 설정
+//     let maxProfit = 0; // 초기 최대 이익을 0으로 설정
 
+//     for (let i = 0; i < prices.length; i++) {
+//         // 현재 가격이 현재까지의 최소 가격보다 작은 경우 최소 가격 갱신
+//         if (prices[i] < minPrice) {
+//             minPrice = prices[i];
+//         }
+//         // 현재 가격에서 최소 가격을 뺀 값이 현재까지의 최대 이익보다 큰 경우 최대 이익 갱신
+//         else if (prices[i] - minPrice > maxProfit) {
+//             maxProfit = prices[i] - minPrice;
+//         }
+//         // 이 외의 경우는 특별히 할 것이 없음 (단순히 다음 반복으로 넘어감)
+//     }
+
+//     return maxProfit; // 최대 이익 반환
+// };
+
+//gpt 답 
 
 
