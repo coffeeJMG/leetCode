791c5471e4a989a923dbff231e98123a10ec4a8a
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-05-23 15:03:25 +0900

    Time: 53 ms (49.27%), Space: 48.3 MB (87.54%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.js
+++ 1025-divisor-game/1025-divisor-game.js
@@ -1,3 +1,36 @@
+// var divisorGame = function(n) {
+    
+    
+//        let count =0 ; // count % 2 ===1 이면 Alice Turn
+    
+    
+//     while( n > 1 ){
+        
+        
+//         for(let i=n-1; i>=0; i--){
+           
+//             if( n % i ===0){
+                
+//                 count ++ // 자기 턴 종료하면서 +1 
+//                 n = n-i       
+             
+//                 break;
+//             }
+           
+//        }
+        
+    
+//     }
+        
+  
+//     if(count % 2 ===1 ){
+        
+//         return true
+//     }else return false
+    
+// };
+
+
 var divisorGame = function(n) {
     let count = 0; // Alice의 턴을 추적 (짝수일 때 Alice의 차례)
     
