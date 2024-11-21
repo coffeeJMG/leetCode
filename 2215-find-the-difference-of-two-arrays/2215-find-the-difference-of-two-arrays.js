6fa783a424ddee9801c09aba83a785ba77012535
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-11-21 16:00:13 +0900

    Time: 7 ms (91.00%), Space: 56 MB (63.18%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,16 +4,12 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    
-    nums1 = Array.from(new Set(nums1));
-    nums2 = Array.from(new Set(nums2));
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
 
-    
-    const onlyInNums1 = nums1.filter(num => !nums2.includes(num));
+    // 차집합 계산 시 Set의 has 메서드를 사용
+    const onlyInNums1 = [...set1].filter(num => !set2.has(num));
+    const onlyInNums2 = [...set2].filter(num => !set1.has(num));
 
-    
-    const onlyInNums2 = nums2.filter(num => !nums1.includes(num));
-
-    
     return [onlyInNums1, onlyInNums2];
 };
