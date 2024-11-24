b54e9bc69c2d345d7d8b72ff0881f53bdd580f7d
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-11-24 12:19:45 +0900

    Time: 0 ms (100.00%), Space: 52.6 MB (26.77%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,16 +3,35 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    let totalSum = nums.reduce((acc, curr) => acc + curr, 0); // 배열 전체 합
-    let leftSum = 0; // 현재 인덱스 기준 누적 합 (왼쪽 합)
-
-    for (let i = 0; i < nums.length; i++) {
-        let rightSum = totalSum - leftSum - nums[i]; // 우측 합 계산
-        if (leftSum === rightSum) {
-            return i; // 피벗 인덱스 반환
-        }
-        leftSum += nums[i]; // 왼쪽 합 갱신
+  
+     
+    let totalSum = 0;
+    let leftSum =0;
+    
+    for(let i=0; i<nums.length; i++){
+        
+        totalSum +=nums[i]
     }
-
-    return -1; // 피벗 인덱스가 없으면 -1 반환
+    
+    for(let i=0; i<nums.length; i++){
+        
+        
+        totalSum -= nums[i]
+        
+        if(leftSum === totalSum ){
+            
+            return i;
+        }
+        
+        leftSum+=nums[i]
+        
+    }
+       
+    return -1
 };
+
+
+
+// 총 배열의 합을 계산
+// 우측의합은 총배열의 합 - num[i] 누적
+// leftSum = 우측의합인 위 계산과 일치하면 Index 반환
