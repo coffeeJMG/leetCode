1f3e0997ebaf074ba5f135b43c9e66183d115670
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-11-24 11:13:50 +0900

    Time: 1 ms (77.12%), Space: 50.4 MB (10.95%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -5,31 +5,27 @@
  */
 var isSubsequence = function(s, t) {
     
-            let answer =true;
-            let result =""
-            let newS = s
+    
+        let index = 0;
+        let string = ""
+    
+        if(s==="") return true
+        
+        for(let i=0; i<t.length; i++){
             
-            for(let i=0; i<t.length; i++){
+            if(t[i] === s[index]){
                 
-                let item =t[i]
-                
-                if(s.includes(t[i]) && t[i] === newS[0]){
-                    
-                    result +=t[i]
-                    newS = newS.slice(1)
-                }    
-                
+                string +=t[i]
+                index ++
             }
-    
-    console.log(result)
-    
-    
-    if(result !== s){
+            
+            if(string.length === s.length ){
+                
+                return true;
+            }
+            
+        }
         
         return false
-    }
-    
-    return answer
-    
     
 };
