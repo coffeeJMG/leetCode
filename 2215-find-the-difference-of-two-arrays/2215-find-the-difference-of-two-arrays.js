c1d4aade8e5e4b0ea5e864b79509ccb7de52e373
정민규 <114897197+coffeeJMG@users.noreply.github.com>
2024-11-25 23:04:48 +0900

    Time: 7 ms (91.14%), Space: 56.7 MB (36.08%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,12 +4,28 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    const set1 = new Set(nums1);
-    const set2 = new Set(nums2);
-
-    // 차집합 계산 시 Set의 has 메서드를 사용
-    const onlyInNums1 = [...set1].filter(num => !set2.has(num));
-    const onlyInNums2 = [...set2].filter(num => !set1.has(num));
-
-    return [onlyInNums1, onlyInNums2];
+    
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+    
+    const array1 = [...set1].filter(x=> !set2.has(x))
+    const array2 = [...set2].filter(x=> !set1.has(x))
+    
+    
+    return [array1,array2]
+    
 };
+
+
+
+// // 교집합
+// const intersection = new Set([...setA].filter(x => setB.has(x)));
+// console.log(intersection); // Set { 2, 3 }
+
+// // 합집합
+// const union = new Set([...setA, ...setB]);
+// console.log(union); // Set { 1, 2, 3, 4 }
+
+// // 차집합
+// const difference = new Set([...setA].filter(x => !setB.has(x)));
+// console.log(difference); // Set { 1 }
